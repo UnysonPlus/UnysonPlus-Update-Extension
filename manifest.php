2fca811c04e9dd89b1d440b0d021f52feee76999
jonmlas <jonmlas@gmail.com>
2025-08-21 21:30:37 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -11,7 +11,7 @@ $manifest['description'] = __(
     'fw' 
 );
 
-$manifest['version']     = '1.0.12';
+$manifest['version']     = '1.0.13';
 $manifest['display']     = true;
 $manifest['standalone']  = true;
 
